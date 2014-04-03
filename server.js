7d3413bf3542f2a8996b6be9b2f85d0158788079
Hack Reactor Students <accounts@hackreactor.com>
2014-04-03 11:33:05 -0700

    finished first version of mongo db

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 var express = require('express');
-var db = require('./db_config.js');
+var db = require('./config/db_config.js');
+
 var app = express();
 var port = process.env.PORT || 8080;
 
-app.configure(function() {
-  app.use(express.static(__dirname + '/app'));
-});
+app.use(express.static(__dirname + '/app'));
 
 app.get('/', function(req, res) {
   res.render('/views/main.html');
