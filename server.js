065c48ddbe56d63ef7daa8add18bd7d3bd67c165
jontewks <jontewks@gmail.com>
2014-04-02 20:58:15 -0700

    Added basic express server.

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+var app = express();
+var port = process.env.PORT || 8080;
+
+app.configure(function() {
+  app.use(express.static(__dirname + '/app'));
+});
+
+app.get('/', function(req, res) {
+  res.render('/views/main.html');
+});
+
+app.listen(port);
+console.log('Listening on ' + port);
