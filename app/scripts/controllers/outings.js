b07ee31444d094ec99f1bc238a6596e79ed905da
Hack Reactor Students <accounts@hackreactor.com>
2014-04-03 11:32:28 -0700

    Modularize controllers. Correct slide toggle.

---
+++ app/scripts/controllers/outings.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var dash = angular.module('moviebuddyApp');
+
+dash.controller('outingsController', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/profile.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var dash = angular.module('moviebuddyApp');
+
+dash.controller('profileController', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
