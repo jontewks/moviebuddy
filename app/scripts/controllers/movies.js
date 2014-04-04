ee98f387c3fff8ed869eb7a338a135cea0d432a1
Hack Reactor Students <accounts@hackreactor.com>
2014-04-03 20:51:56 -0700

    Introduce Rotten Tomatoes API.

--- app/scripts/controllers/movies.js
+++ app/scripts/controllers/movies.js
@@ -2,10 +2,15 @@
 
 var dash = angular.module('moviebuddyApp');
 
-dash.controller('moviesController', function ($scope) {
+dash.controller('moviesController', function ($scope, $http) {
+
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
   });
+
+
+
+
