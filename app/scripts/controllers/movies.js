2a24d77797bfc1cd55e9d0008fc0f0d8b3b3de22
Hack Reactor Students <accounts@hackreactor.com>
2014-04-04 12:28:58 -0700

    Refactor movies controller RT request w pagination.

--- app/scripts/controllers/movies.js
+++ app/scripts/controllers/movies.js
@@ -2,27 +2,35 @@
 
 var app = angular.module('moviebuddyApp');
 
-app.controller('moviesController', function ($scope, $http) {
+app.service('getMoviesData', function($http){
+  var rottenTomatoesUrl = 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json?callback=JSON_CALLBACK&apikey=63za93cgdtv88ves8p6d9wrk';
+  var pageLimitQuery = '&page_limit=';
+  var pageQuery = '&page=';
+  this.getMovieData = function(page, pageLimit) {
+    page = page || 1;
+    pageLimit = pageLimit || 20;
+    var query = rottenTomatoesUrl + pageLimitQuery + pageLimit + pageQuery + page;
+    return $http.jsonp(query);
+  };
+});
 
-    var rottenTomatoesUrl = 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json?callback=JSON_CALLBACK&page_limit=50&apikey=63za93cgdtv88ves8p6d9wrk';
+app.controller('moviesController', function ($scope, $http, getMoviesData) {
 
-    $http.jsonp(rottenTomatoesUrl)
-    .then(function(data){
-      var rtData = data.data;
-      $scope.movies = rtData.movies;
-      $scope.moviePosters = [];
-      for (var i = 0; i < $scope.movies.length; i++){
-        $scope.moviePosters.push($scope.movies[i].posters.thumbnail);
-      }
-    });
-
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  getMoviesData.getMovieData()
+  .then(function(data){
+    var rtData = data.data;
+    console.log(rtData);
+    $scope.movies = rtData.movies;
+    // $scope.moviePosters = [];
+    // for (var i = 0; i < $scope.movies.length; i++){
+    //   $scope.moviePosters.push($scope.movies[i].posters.thumbnail);
+    // }
   });
 
+  $scope.awesomeThings = [
+    'HTML5 Boilerplate',
+    'AngularJS',
+    'Karma'
+  ];
 
-
-
+});
