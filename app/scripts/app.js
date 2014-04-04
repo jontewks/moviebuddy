34350c0474096c4fd8f628e74c1c43f484221352
Hack Reactor Students <accounts@hackreactor.com>
2014-04-03 20:59:43 -0700

    Added facebook authentication and tested connectivity with API. Added login button for testing purposes.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -13,4 +13,82 @@ angular
       .otherwise({
         redirectTo: '/'
       });
-  });
+  })
+  .directive('fb', ['$FB', function($FB) {
+    return {
+      restrict: "E",
+      replace: true,
+      template: "<div id='fb-root'></div>",
+      compile: function(tElem, tAttrs) {
+        return {
+          post: function(scope, iElem, iAttrs, controller) {
+            var fbAppId = iAttrs.appId || '';
+            var fb_params = {
+              appId: iAttrs.appId || "",
+              cookie: iAttrs.cookie || true,
+              status: iAttrs.status || true,
+              xfbml: iAttrs.xfbml || true
+            };
+
+            // Setup the post-load callback
+            window.fbAsyncInit = function() {
+              $FB._init(fb_params);
+              if('fbInit' in iAttrs) {
+                  iAttrs.fbInit();
+              }
+            };
+
+            (function(d, s, id, fbAppId) {
+              var js, fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) return;
+              js = d.createElement(s); js.id = id; js.async = true;
+              js.src = "//connect.facebook.net/en_US/all.js";
+              fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk', fbAppId));
+          }
+        };
+      }
+    };
+  }])
+  .factory('$FB', ['$rootScope', function($rootScope) {
+    var fbLoaded = false;
+
+    // Our own customisations
+    var _fb =  {
+      loaded: fbLoaded,
+      isLoaded : function(){
+          return this.loaded;
+      },
+      authenticated : false,
+      isAuthenticated : function(){
+          return this.authenticated;
+      },
+      _init: function(params) {
+        var self = this;
+        if(window.FB) {
+
+          // FIXME: Ugly hack to maintain both window.FB
+          // and our AngularJS-wrapped $FB with our customisations
+          angular.extend(window.FB, this);
+          angular.extend(this, window.FB);
+
+          // Set the flag
+          this.loaded = true;
+
+          // Initialise FB SDK
+          window.FB.init(params);
+          window.FB.Event.subscribe('auth.authResponseChange', function(response) {
+            if (response.status === 'connected') {
+              self.authenticated = true;
+            }
+          });
+
+          if(!$rootScope.$$phase) {
+            $rootScope.$apply();
+          }
+        }
+      }
+    };
+
+    return _fb;
+  }]);
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var db = require('./config/db_config.js');
+var passport = require('passport');
 
 var app = express();
 var port = process.env.PORT || 8080;
@@ -9,6 +10,18 @@ app.use(express.static(__dirname + '/app'));
 app.get('/', function(req, res) {
   res.render('/views/main.html');
 });
+
+app.get('/login', function(req, res) {
+  res.render('/views/login.html');
+});
+
+app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
+
+// handle the callback after facebook has authenticated the user
+app.get('/auth/facebook/callback', passport.authenticate('facebook', {
+  successRedirect : '/',
+  failureRedirect : '/login'
+}));
 
 app.listen(port);
 console.log('Listening on ' + port);
