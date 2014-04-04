8bd7a805da55caf105f2069a39d767e32b78e93a
Hack Reactor Students <accounts@hackreactor.com>
2014-04-04 14:30:38 -0700

    next and prev page functions functional

--- app/scripts/controllers/movies.js
+++ app/scripts/controllers/movies.js
@@ -6,22 +6,53 @@ app.service('getMoviesData', function($http){
   var rottenTomatoesUrl = 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json?callback=JSON_CALLBACK&apikey=63za93cgdtv88ves8p6d9wrk';
   var pageLimitQuery = '&page_limit=';
   var pageQuery = '&page=';
-  this.getMovieData = function(page, pageLimit) {
-    page = page || 1;
-    pageLimit = pageLimit || 20;
+  this.getMovieData = function(page) {
+    // page = page || 1;
+    var pageLimit = 20;
     var query = rottenTomatoesUrl + pageLimitQuery + pageLimit + pageQuery + page;
     return $http.jsonp(query);
   };
 });
 
 app.controller('moviesController', function ($scope, $http, getMoviesData) {
+  $scope.movies;
+  $scope.page = 1;
+  $scope.totalMovies;
 
-  getMoviesData.getMovieData()
+  // get movie data
+  getMoviesData.getMovieData($scope.page)
   .then(function(data){
     var rtData = data.data;
-    console.log(rtData);
     $scope.movies = rtData.movies;
+    $scope.totalMovies = rtData.total;
   });
+
+  // go to the next page
+  $scope.nextPage = function() {
+
+    var lastPage = Math.ceil($scope.totalMovies / 20);
+    if ($scope.page === lastPage) { return;}
+
+    getMoviesData.getMovieData(++$scope.page)
+    .then(function(data) {
+      var rtData = data.data;
+      $scope.movies = rtData.movies;
+    });
+
+  };
+
+  // go to the next page
+  $scope.prevPage = function() {
+
+    if ($scope.page === 1) { return; }
+
+    getMoviesData.getMovieData(--$scope.page)
+    .then(function(data) {
+      var rtData = data.data;
+      $scope.movies = rtData.movies;
+    });
+
+  };
 
   $scope.awesomeThings = [
     'HTML5 Boilerplate',
