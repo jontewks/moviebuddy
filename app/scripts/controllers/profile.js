85fef48b90db6d8dd7876bbd20582475bda2d5a0
Josh <jsgoldberg90@gmail.com>
2014-04-08 11:49:09 -0700

    editable profile fields

--- app/scripts/controllers/profile.js
+++ app/scripts/controllers/profile.js
@@ -12,35 +12,43 @@ app.service('getUsers', function($http) {
       url: '/api/user/' + facebookId
     });
   };
+});
 
+app.service('updateUser', function($http){
+  this.updateUser = function(facebookId, userObj) {
+    return $http({
+      method : 'POST',
+      url    : '/api/updateUser',
+      data   : { user: userObj }
+    });
+  };
 });
 
 // This controller controls the profile.
 app.controller('ProfileController', function ($scope, $rootScope,getUsers) {
 
   // *** Want to grab this upon authentication. ***
-  $scope.testUser;
-  $scope.testHometown;
-  $scope.testFavMovie;
-  $scope.testFavGenre;
-  $scope.testAge;
-  $scope.testFavTheater;
-  $scope.testCurrentCity;
-  $scope.testFavActor;
-  $scope.profilePicture;
+  $scope.User;
+  $scope.Hometown;
+  $scope.FavMovie;
+  $scope.FavGenre;
+  $scope.Age;
+  $scope.FavTheater;
+  $scope.CurrentCity;
+  $scope.FavActor;
   $scope.profilePicture = 'http://graph.facebook.com/'+$rootScope.user.facebookId+'/picture?type=large';
   // *** Want to nest this in a promise or callback. ***
   getUsers.getUser($rootScope.user.facebookId)
   .then(function(data) {
     var user = data.data;
-    $scope.testUser         = user.name;
-    $scope.testHometown     = user.hometown;
-    $scope.testFavMovie     = user.favMovie;
-    $scope.testFavGenre     = user.favGenre;
-    $scope.testAge          = user.age;
-    $scope.testFavTheater   = user.favTheater;
-    $scope.testCurrentCity  = user.currentCity;
-    $scope.testFavActor     = user.favActor;
+    $scope.User         = user.name;
+    $scope.Hometown     = user.hometown;
+    $scope.FavMovie     = user.favMovie;
+    $scope.FavGenre     = user.favGenre;
+    $scope.Age          = user.age;
+    $scope.FavTheater   = user.favTheater;
+    $scope.CurrentCity  = user.currentCity;
+    $scope.FavActor     = user.favActor;
   });
 
 });
