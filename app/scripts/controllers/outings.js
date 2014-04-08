c7ed0de34516f6989688d9cec538347403ad8edb
Janas Page <jp@janaspage.com>
2014-04-07 18:22:52 -0700

    Form data now adds to 'outings' list.

--- app/scripts/controllers/outings.js
+++ app/scripts/controllers/outings.js
@@ -7,29 +7,8 @@ app.controller('OutingsController', function ($scope, $http) {
 
   var newOutingButtonVisible = true;
   var newOutingFormVisible = false;
-  $scope.form = {};
 
-  $scope.showNewOutingButton = function() {
-    return newOutingButtonVisible;
-  };
-
-  $scope.showNewOutingForm = function() {
-    return newOutingFormVisible;
-  };
-
-  // Hide the 'new outing' button; show the 'new outing' form.
-  $scope.newOuting = function() {
-    newOutingButtonVisible = false;
-    newOutingFormVisible = true;
-  };
-
-  // Hide the 'new outing' form; show the 'new outing' button.
-  $scope.cancelNewOuting = function() {
-    newOutingFormVisible = false;
-    newOutingButtonVisible = true;
-  };
-
-  // Create a new 'outing' object from passed form and user.
+  // Function to create new 'outing' object from form and user.
   var createOuting = function(form, userId) {
     var outing = {};
     outing.movie = form.movie;
@@ -43,7 +22,30 @@ app.controller('OutingsController', function ($scope, $http) {
     return outing;
   };
 
-  // Process the 'new outing' form.
+  // Define empty object to hold 'new outing' form data.
+  $scope.form = {};
+
+  $scope.showNewOutingButton = function() {
+    return newOutingButtonVisible;
+  };
+
+  $scope.showNewOutingForm = function() {
+    return newOutingFormVisible;
+  };
+
+  // Function to hide 'new outing' button, show 'new outing' form.
+  $scope.newOuting = function() {
+    newOutingButtonVisible = false;
+    newOutingFormVisible = true;
+  };
+
+  // Function to hide 'new outing' form; show 'new outing' button.
+  $scope.cancelNewOuting = function() {
+    newOutingFormVisible = false;
+    newOutingButtonVisible = true;
+  };
+
+  // Function to process 'new outing' form.
   $scope.processOutingForm = function() {
     var outing = createOuting($scope.form, $scope.userId || 1234);
     $http({
@@ -53,10 +55,11 @@ app.controller('OutingsController', function ($scope, $http) {
     })
     .success(function(data) {
       console.log('Successfully posted:', data);
+      // *** Temporarily push to local array until DB is up.
+      $scope.outings.push(outing);
       // Hide the 'new outing' form, show the 'new outing' button.
       newOutingFormVisible = false;
       newOutingButtonVisible = true;
-      // *** Call a 'displayOutings' or 'refreshOutings' or 'appendOuting' function?
     })
     .error(function(data, status, headers, config) {
       console.log('Error processing "new outing" form:', data, status, headers, config);
@@ -64,10 +67,11 @@ app.controller('OutingsController', function ($scope, $http) {
     });
   };
 
-  // Pull from the DB outings that are visible to the user.
+  // Function to pull from DB 'outings' for user.
   $scope.getOutings = function(userId) {
     userId = userId || 1234;
-    console.log('In getOutings(), userId is', userId);
+    console.log('In getOutings()', $scope.outings);
+    // *** Temporarily return only dummy data until DB is up.
     // return $http({
     //   method: 'GET',
     //   url: '/api/outings/' + userId
@@ -80,8 +84,7 @@ app.controller('OutingsController', function ($scope, $http) {
     //   console.log('Error getting outings from DB:', data, status, headers, config);
     //   // *** Throw an 'error' object?
     // });
-    // *** Returning only dummy data below.
-    return [{
+    return $scope.outings || [{
       outingId:      5678,
       movie:         'Ride Along',
       date:          '2014-04-12',
@@ -90,9 +93,9 @@ app.controller('OutingsController', function ($scope, $http) {
       city:          'San Francisco',
       state:         'CA',
       zip:           94115,
-      invitees:      [],
-      attendees:     [],
-      creator:       1234 // A user id (foreign key).
+      invitees:      [1234, 1111, 2222], // User IDs (foreign keys).
+      attendees:     [1234, 1111], // User IDs (foreign keys).
+      creator:       1234 // A user ID (foreign key).
     }, {
       outingId:      8765,
       movie:         'Divergent',
@@ -102,18 +105,13 @@ app.controller('OutingsController', function ($scope, $http) {
       city:          'San Francisco',
       state:         'CA',
       zip:           94103,
-      invitees:      [],
-      attendees:     [],
-      creator:       4321 // A user id (foreign key).
+      invitees:      [], // User IDs (foreign keys).
+      attendees:     [], // User IDs (foreign keys).
+      creator:       4321 // A user ID (foreign key).
     }];
   };
 
+  // Initialize list of outings.
   $scope.outings = $scope.getOutings($scope.userId);
-
-  $scope.awesomeThings = [
-    'HTML5 Boilerplate',
-    'AngularJS',
-    'Karma'
-  ];
 
 });
