76f6742ff9dc50b6c30f91436bd1693949d750fc
Janas Page <jp@janaspage.com>
2014-04-16 17:07:58 -0700

    Using ng for iOSy view buttons.

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -3,7 +3,42 @@
 var app = angular.module('moviebuddyApp');
 
 app.controller('DashController', ['$scope', 'authentication', function ($scope, authentication) {
-	$scope.logout = function() {
+
+  var outingsVisible = true;
+  var moviesVisible = false;
+  var profileVisible = false;
+
+  $scope.showOutings = function() {
+    return outingsVisible;
+  };
+
+  $scope.showMovies = function() {
+    return moviesVisible;
+  };
+
+  $scope.showProfile = function() {
+    return profileVisible;
+  };
+
+  $scope.navToOutings = function() {
+    outingsVisible = true;
+    moviesVisible = false;
+    profileVisible = false;
+  };
+
+  $scope.navToMovies = function() {
+    outingsVisible = false;
+    moviesVisible = true;
+    profileVisible = false;
+  };
+
+  $scope.navToProfile = function() {
+    outingsVisible = false;
+    moviesVisible = false;
+    profileVisible = true;
+  };
+
+  $scope.logout = function() {
     authentication.fbLogout();
   };
 }]);
